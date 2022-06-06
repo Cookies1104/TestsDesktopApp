4c926008f2c40b1d3cf2f817b92807af98318420
Cookies1104 <ikqmamba@gmail.com>
2022-06-06 17:29:29 +0400

    Добавлены позитивные тесты полей номер/дата составления/составлена в ценах по состоянию на/ в сводке затрат

--- utils.py
+++ utils.py
@@ -3,3 +3,7 @@ from pywinauto.application import WindowSpecification
 
 def close_window(window: WindowSpecification) -> None:
     window.wrapper_object().close()
+
+
+def clear_field(edit_field: WindowSpecification) -> None:
+    edit_field.type_keys('{HOME} ^a {DEL}')
